028a78563f1c89a5bd30db15fc4d02860a8febbd
Milan <milan.zivanov@gmail.com>
2016-11-21 15:34:36 +0100

    browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
 "use strict"
 var gulp = require("gulp"),
     uglify = require("gulp-uglify"),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 // Scripts task
 // Uglify task
 gulp.task("scripts", function() {
     gulp.src("js/*.js")
     .pipe(uglify())
-    .pipe(gulp.dest("build/js"));    
+    .pipe(gulp.dest("build/js"));
 })
 // style task
 gulp.task('sass', function () {
   return gulp.src('sass/*.scss')
     .pipe(sass({outputStyle: 'expended'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
 });
+// browswer sync server
+gulp.task("server", function() {
+  browserSync.init({
+    server: "./"
+  });
+});
+
 // watch task
 gulp.task("watch", function() {
+    // liverelaod
     gulp.watch("js/*.js", ["scripts"]);
     gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("*.html").on("change", reload);
 });
 // default
-gulp.task("default", ["scripts", "sass", "watch"]);
-
-
+gulp.task("default", ["server","scripts", "sass", "watch"]);
