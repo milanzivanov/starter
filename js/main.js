f7699a79457880fee87e87fb53de41236c46747f
Milan <milan.zivanov@gmail.com>
2016-12-02 13:11:19 +0100

    popup and dropdow level 3

--- js/main.js
+++ js/main.js
@@ -32,6 +32,14 @@ $(function() {
   $('select').niceSelect();
 
 
+  // Example 1: From an element in DOM
+  $('.open-popup-link').magnificPopup({
+    type:'inline',
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+
+
+
 });
 
 // loader
