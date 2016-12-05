674e258521ada884aec768308f6425a37a9c0f72
Milan <milan.zivanov@gmail.com>
2016-12-05 14:46:13 +0100

    test git reset [commit]

--- main.js
+++ main.js
@@ -3,3 +3,14 @@
 //     return (arg1 * arg2);
 // }
 // testGulp(10, 10);
+
+// javascript snippets
+//'use strict';
+// getElementsByClassName('className');
+// for (var i = 0; i < array.length; i++) {
+//     array[i]
+//
+// };
+// setTimeout(function () {
+//
+// }, 10);
